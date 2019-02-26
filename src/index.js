fdb8d60d5c0c30e90779f30db78c6938366bb7bd
alexgrist14 <agurec.fasasf@gmail.com>
2019-02-26 21:22:53 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  // Массив в котором число разделено на делители
+   let divided=dividing(base);
+   // Массив хранящий возможное кол-во нулей
+   let zerosArr = [];
+   for(let i=0; i<divided.length;i++)
+   {
+     // Рассчитываем кол-во нулей на конце по формуле
+     zerosArr [i]=trailing(number,divided[i]);
+   }
+   let div=1;
+   for(let i=0; i<zerosArr .length;i++)
+   {
+    
+    let repeatedItem=zerosArr [i];
+     if(zerosArr [i+1]==repeatedItem)
+     {
+        div++;
+        zerosArr [i]=Math.floor( zerosArr [i]/div);
+     }
+     else if(zerosArr [i+1]!=zerosArr [i])
+       div=1;
+     
+   }
+   // По формуле минимальное возможное кол-во нулей и есть ответ
+   let min=zerosArr [0];
+   for(let i=0; i<zerosArr .length;i++)
+   {
+     if(min>zerosArr [i]){
+       min=zerosArr [i];
+     }
+   }
+    return min;
+  }
+  // Разбор числа на делители
+  function dividing(base){
+  let primeArr = [];
+      for (let i = 2; base != 1; i++) {
+          if (base % i == 0) {
+              primeArr.push(i);
+              base /= i;
+              i--;
+          }
+      }
+      return primeArr;
+  }
+  // Формула расчёта нулей на конце
+  function trailing(number,divided)
+  {
+    let zeros=0;
+    while(number>0)
+    {
+      number=Math.floor(number/divided);
+      zeros +=number;
+    }
+    return zeros;
+  }
